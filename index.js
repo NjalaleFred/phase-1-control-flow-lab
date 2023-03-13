4d2fc0572218711bfb169e1415d6ea5ebde42490
Njalale <fnjalale@gmail.com>
2023-03-14 00:35:36 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result 
+  if(someValue <= 400){
+    result = 'This one is on me!'
+  }
+  else if (someValue > 2000){
+    result = 'I will gladly take your thirty bucks.'
+  }
+     if (someValue > 2500){
+      result = 'No can do.'
+     }   
+  
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+   return  city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(quantity){
+  switch(quantity){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
